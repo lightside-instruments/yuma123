0c8b907951079ad1b17313e97548fe29ff3b7200
Vladimir Vassilev <vladimir@lightside-instruments.com>
2025-03-23 15:37:14 +0100

    Release 2.14-alpha3

--- netconf/test/netconfd/draft-ietf-bmwg-network-tester-cfg/run.sh
+++ netconf/test/netconfd/draft-ietf-bmwg-network-tester-cfg/run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -e
 
+ip link delete veth1 || true
+ip link add veth1 type veth peer name veth2
+ifconfig veth1 hw ether 6C:A9:6F:00:00:01
+ifconfig veth2 hw ether 6C:A9:6F:00:00:02
+ifconfig veth1 192.0.2.1 up
+ifconfig veth2 192.0.2.1 up
+
 rm -rf tmp || true
 mkdir tmp
 cd tmp
 
-wget https://www.ietf.org/archive/id/draft-ietf-bmwg-network-tester-cfg-05.txt
+#wget https://www.ietf.org/archive/id/draft-ietf-bmwg-network-tester-cfg-05.txt
 
 
 #xml2rfc draft-ietf-bmwg-network-tester-cfg-05.xml
 
-rfcstrip draft-ietf-bmwg-network-tester-cfg-05.txt
+#rfcstrip draft-ietf-bmwg-network-tester-cfg-05.txt
 
 #pyang -f tree --path . ietf-traffic-generator*.yang
 #pyang -f tree --path . ietf-traffic-analyzer*.yang
@@ -34,14 +41,15 @@ else
   rm /tmp/ncxserver.sock || true
 
   /usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules --module=ietf-traffic-generator --module=ietf-traffic-analyzer --no-startup --validate-config-only --superuser=$USER
-  #/usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules --module=ietf-traffic-generator --module=ietf-traffic-analyzer --no-startup --superuser=$USER
-  #SERVER_PID=$!
+  /usr/sbin/netconfd --modpath=.:/usr/share/yuma/modules --module=ietf-interfaces --module=ietf-traffic-generator --module=ietf-traffic-analyzer --no-startup --superuser=$USER &
+  SERVER_PID=$!
   cd ..
 fi
 
-#sleep 3
-#python session.litenc.py --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD
-#python session.yangcli.py --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD
-#kill -KILL $SERVER_PID
-#cat tmp/server.log
-#sleep 1
+sleep 3
+python session.yangcli.py --server=$NCSERVER --port=$NCPORT --user=$NCUSER --password=$NCPASSWORD
+kill -KILL $SERVER_PID
+cat tmp/server.log
+sleep 1
+ip link delete veth1 || true
+
--- netconf/test/platform-validation-scripts/debian.sh
+++ netconf/test/platform-validation-scripts/debian.sh
@@ -46,11 +46,24 @@ debuild -us -uc
 apt-get -y install python3-paramiko python3-lxml
 dpkg -i ../python3-yuma*.deb
 
+cd ~
+rsync -rav yuma123_${ver}/example-modules/ietf-traffic-generator/ yuma123-netconfd-module-ietf-traffic-generator_${ver}
+tar -czvf yuma123-netconfd-module-ietf-traffic-generator_${ver}.orig.tar.gz --exclude .git yuma123-netconfd-module-ietf-traffic-generator_${ver}
+cd yuma123-netconfd-module-ietf-traffic-generator_${ver}
+mk-build-deps  -i -t 'apt-get -y'
+cd ..
+rm -rf yuma123-netconfd-module-ietf-traffic-generator_${ver}
+tar -xzvf yuma123-netconfd-module-ietf-traffic-generator_${ver}.orig.tar.gz
+cd yuma123-netconfd-module-ietf-traffic-generator_${ver}
+debuild -us -uc
+dpkg -i ../*.deb
+
 #testing
 apt-get -y install openssh-client openssh-server
 ssh-keygen -t rsa -b 4096 -m PEM -f ~/.ssh/id_rsa -N ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PubkeyAcceptedAlgorithms=+ssh-rsa"  >> /etc/ssh/sshd_config
 echo "Port 22" >> /etc/ssh/sshd_config
 echo "Port 830" >> /etc/ssh/sshd_config
 echo "Port 1830" >> /etc/ssh/sshd_config
@@ -75,11 +88,6 @@ cd ~/yuma123_${ver}/netconf/test/netconfd
 apt-get -y install python3-ncclient valgrind
 apt-get -y install python-is-python3
 apt-get -y install psmisc # contains killall
-
-apt-get -y install wget
-wget https://raw.githubusercontent.com/mbj4668/rfcstrip/refs/heads/master/rfcstrip
-chmod ugo+x rfcstrip
-export PATH=$PATH:`pwd`
 
 multiarch=$(dpkg-architecture -q DEB_BUILD_MULTIARCH)
 prefix="/usr"
